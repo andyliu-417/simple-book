412660ee1096bd0cac0992dda6669c90a110b816
andyliu-417 <andy2018.liu@gmail.com>
2018-08-26 17:32:09 -0400

    panda init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        panda
+      </div>
+    );
+  }
+}
+
+export default App;
