00c50d38f11d24d78a59ece112b031bf35de6a2f
andyliu-417 <andy2018.liu@gmail.com>
2018-08-26 23:21:01 -0400

    header layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,17 @@ class Header extends Component {
     return (
       <styles.HeaderWrapper>
         <styles.Logo />
+        <styles.Nav>
+          <styles.NavItem className="left active">首页</styles.NavItem>
+          <styles.NavItem className="left">下载App</styles.NavItem>
+          <styles.NavItem className="right">登录</styles.NavItem>
+          <styles.NavItem className="right">Aa</styles.NavItem>
+          <styles.NavSearch/>
+        </styles.Nav>
+        <styles.Addition>
+          <styles.Button className="reg">注册</styles.Button>
+          <styles.Button className="writing">写文章</styles.Button>
+        </styles.Addition>
       </styles.HeaderWrapper>
     );
   }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,25 +1,90 @@
 import styled from "styled-components";
-import logoPic from '../../statics/logo.png';
+import logoPic from "../../statics/logo.png";
 
 const HeaderWrapper = styled.div`
-    height: 56px;
-    border-bottom: 1px solid #f0f0f0;
-    position: relative;
+  height: 56px;
+  border-bottom: 1px solid #f0f0f0;
+  position: relative;
 `;
 
 const Logo = styled.a`
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: inline-block;
-    height: 56px;
-    width: 100px;
-    background: url(${logoPic});
-    background-size: contain;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  height: 56px;
+  width: 100px;
+  background: url(${logoPic});
+  background-size: contain;
+`;
+
+const Nav = styled.div`
+  width: 70%;
+  height: 100%;
+  margin: 0 auto;
+`;
+
+const NavItem = styled.div`
+  height: 56px;
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+    color: #969696;
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+`;
+
+const NavSearch = styled.input.attrs({ placeholder: "搜索" })`
+  width: 160px;
+  height: 38px;
+  margin: 9px 0 0 20px;
+  padding: 0 20px;
+  border: none;
+  border-radius: 19px;
+  background: #eee;
+  font-size: 14px;
+  &::placeholder {
+    color: #999;
+  }
+`;
+
+const Addition = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 56px;
+`;
+
+const Button = styled.div`
+  display: inline-block;
+  line-height: 38px;
+  border-radius: 19px;
+  border:1px solid #ec6149;
+  margin: 9px 20px 9px 0;
+  padding: 0 20px;
+  font-size: 14px;
+  &.reg {
+      color: #ec6149;
+  }
+  &.writing {
+      color: #fff;
+      background: #ec6149;
+  }
 `;
 
 export const styles = {
-    HeaderWrapper,
-    Logo
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button
 };
-
