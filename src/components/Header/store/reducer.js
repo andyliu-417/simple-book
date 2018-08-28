e7e4855463db219a9e6cd95825586930c1862ae3
andyliu-417 <andy2018.liu@gmail.com>
2018-08-28 17:08:41 -0400

    function object

--- src/components/Header/store/reducer.js
+++ src/components/Header/store/reducer.js
@@ -3,26 +3,28 @@ import * as actionTypes from "./actionType";
 const defaultState = { foo: "foo" };
 
 const reducer_handlers = {
-    [ACTION_TYPE_1](state, action) {
-      console.log('return action 1');
-    },
-    [ACTION_TYPE_2](state, action) {
-      console.log('return action 2');
-    }
+  [actionTypes.FOO]: (state, action) => {
+    console.log("reducer:", action.type);
+    return state;
   }
-  
+};
+
 export default (state = defaultState, action) => {
-    if(action.type) {
-        reducer_handlers[action.type]();
-      }
-  switch (action.type) {
-    case actionTypes.FOO:
-      console.log("reducer:", action.type);
-      return state;
-    case "RESULT":
-      console.log("reducer:", action.type);
-      return {foo: action.data};
-    default:
-      return state;
+  if (action.type) {
+    console.log(reducer_handlers);
+    console.log(reducer_handlers[action.type]);
+    console.log(action.type == actionTypes.FOO);
+
+    return reducer_handlers[actionTypes.FOO](state, action);
   }
+  // switch (action.type) {
+  //   case actionTypes.FOO:
+  //     console.log("reducer:", action.type);
+  //     return state;
+  //   case "RESULT":
+  //     console.log("reducer:", action.type);
+  //     return {foo: action.data};
+  //   default:
+  //     return state;
+  // }
 };
