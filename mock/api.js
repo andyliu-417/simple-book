0e1df8a4987c90270c3d6047baa6621add71e3fe
andyliu-417 <andy2018.liu@gmail.com>
2018-09-13 10:17:36 -0400

    mock

--- mock/api.js
+++ mock/api.js
@@ -1,35 +1,16 @@
-const users = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 25,
-    address: "New York No. 1 Lake Park"
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 25,
-    address: "London No. 1 Lake Park"
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 25,
-    address: "Sidney No. 1 Lake Park"
-  }
-];
 var mock = require("mockjs");
 
 getUsers = (req, res) => {
-  users = mock.mock({
-    "array|1-10": [
+  const data = mock.mock({
+    "users|10": [
       {
-        id: mock.mock("id|+1:0"),
-        name: mock.mock("@name")
+        "id|+1": 0,
+        "name": "@name",
+        "age|20-30": 25,
       }
     ]
   });
-  res.json(users);
+  res.json(data.users);
 };
 
 module.exports = {
