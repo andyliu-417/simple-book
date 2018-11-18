ef4d747a1201d40f99f0b807be6a1b01f527bae8
andyliu-417 <andy2018.liu@gmail.com>
2018-11-18 11:14:40 -0500

    full addrss or address

--- src/pages/Home/Topic.js
+++ src/pages/Home/Topic.js
@@ -5,10 +5,18 @@ import { TopicWrapper, TopicItem } from "./style";
 import { Divider } from "antd";
 
 class Topic extends PureComponent {
+  state = {
+    // full_address: "full address",
+    full_address: "",
+    address: "address"
+  };
   render() {
     const { topicList } = this.props;
+    const { full_address, address } = this.state;
     return (
       <Fragment>
+        {/* <div>{full_address ? full_address : address}</div> */}
+        <div>{full_address || address}</div>
         <TopicWrapper>
           {topicList.map(item => (
             <TopicItem key={item.get("id")}>
